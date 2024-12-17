d0837ac41d3f000a8730fe93630286b00d36573d
dkmbugua <dkmbugua70@gmail.com>
2024-12-17 18:26:58 +0300

    try

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', () => {
   const uname = process.stdin.read();
-
   if (uname) {
-    process.stdout.write(`Your name is: ${uname}`);
+    process.stdout.write(`Your name is: ${uname.trim()}\n`);
   }
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
