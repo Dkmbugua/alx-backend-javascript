7c1aad85ee565299e7c9045032cfd4aa0f2a9640
dkmbugua <dkmbugua70@gmail.com>
2024-12-18 13:43:11 +0300

    try

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,42 +1,37 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   try {
-    const results = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
-    const lines = results;
-    let i = 0;
-    let countStudents = 0;
-    const fields = {};
-
-    for (const line of lines) {
-      if (line.trim() !== '' && i > 0) {
-        countStudents += 1;
-        const [fname, lname, age, field] = line.split(','); // eslint-disable-line
-        if (!fields[field]) {
-          fields[field] = {
-            count: 1,
-            students: [fname],
-          };
+    const content = fs.readFileSync(fileName, 'utf-8');
+    const lines = content.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
         } else {
-          const newCount = fields[field].count + 1;
-          const newStudents = (fields[field].students).concat(fname);
-          fields[field] = {
-            count: newCount,
-            students: newStudents,
-          };
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
         }
       }
-      i += 1;
     }
-
-    console.log(`Number of students: ${countStudents}`);
-    for (const field of Object.keys(fields)) {
-      const n = fields[field].count;
-      const names = fields[field].students.join(', ');
-      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
     }
   } catch (error) {
-    throw new Error('Cannot load the database');
+    throw Error('Cannot load the database');
   }
 }
 
--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,14 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const host = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello ALX!');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX School!');
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+app.listen(port, host, () => {});
 
 module.exports = app;
