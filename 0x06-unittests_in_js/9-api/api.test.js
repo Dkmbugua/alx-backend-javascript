bd1beeb5eb9bca127e5cd633c6f3903b3a5e173e
dkmbugua <dkmbugua70@gmail.com>
2024-12-29 12:37:20 +0300

    try

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,57 +1,56 @@
-// api.test.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('./api');
+const server = require('./api');
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('Index page', () => {
-    it('should return status code 200', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                done();
-            });
+describe('Integration Testing', () => {
+  // Test for the root route
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.equal('Welcome to the payment system');
+          done();
+        });
+    });
+  });
+
+  // Test suite for the cart route
+  describe('GET /cart/:id', () => {
+    it('should return status code 200 and correct message for a valid numeric id', (done) => {
+      chai.request(server)
+        .get('/cart/12')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.equal('Payment methods for cart 12');
+          done();
+        });
     });
 
-    it('should return the correct message', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res.text).to.equal('Welcome to the payment system');
-                done();
-            });
-    });
-});
-
-describe('Cart page', () => {
-    it('should return status code 200 when id is a number', (done) => {
-        chai.request(app)
-            .get('/cart/12')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.text).to.equal('Payment methods for cart 12');
-                done();
-            });
+    it('should return status code 404 for an invalid id (non-numeric)', (done) => {
+      chai.request(server)
+        .get('/cart/hello')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
     });
 
-    it('should return status code 404 when id is not a number', (done) => {
-        chai.request(app)
-            .get('/cart/hello')
-            .end((err, res) => {
-                expect(res).to.have.status(404);
-                done();
-            });
+    it('should return status code 404 for a missing id', (done) => {
+      chai.request(server)
+        .get('/cart/')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
     });
+  });
 
-    it('should return status code 404 for missing id', (done) => {
-        chai.request(app)
-            .get('/cart/')
-            .end((err, res) => {
-                expect(res).to.have.status(404);
-                done();
-            });
-    });
+  after(() => {
+    server.close(); // Close the server after tests
+  });
 });
