69fb11ef092d02fd4716ea081ec45259d4baa600
dkmbugua <dkmbugua70@gmail.com>
2024-12-29 11:55:46 +0300

    try

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,17 +1,13 @@
-// api.js
 const express = require('express');
-
 const app = express();
 const port = 7865;
 
-// Define the route for the root endpoint
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+  res.end('Welcome to the payment system');
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
 
-module.exports = app; // Export the app for testing
+module.exports = server; // Export the server instead of the app
