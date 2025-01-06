65c3dd37b35bdca50ce26fb606f0bd16e10507d9
dkmbugua <136028413+Dkmbugua@users.noreply.github.com>
2025-01-07 01:24:28 +0300

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const app = express();
-const port = 7865;
+const PORT = 7865;
 
-app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
 });
 
-const server = app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
-module.exports = server; // Export the server instead of the app
+module.exports = app;
